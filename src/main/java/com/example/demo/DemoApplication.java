fa77948173f83ee437b28314511027de6d3f79ad
neil <ws9502@gmail.com>
2022-02-07 17:19:20 +0800

    first_push

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.example.demo","com.example.model","com.example.controller","com.example.service","com.example.repository"})
+@EntityScan({"com.example.demo","com.example.model","com.example.controller","com.example.service","com.example.repository"})
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
